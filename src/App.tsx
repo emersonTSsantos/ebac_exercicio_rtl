d17512b060b37e311f4b41a6b414decc1cbe4cb3
Emerson Teixeira <emerson@exemplo.com>
2024-08-28 15:17:15 -0300

    Aplicando requisitos de testes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://s1.cdn.autoevolution.com/images/news/gallery/1989-batmobile-gets-full-restoration-batman-says-it-s-his-favorite_3.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+        render(<PostComment />);
+        expect(screen.getByTestId('submit-button')).toBeInTheDocument();
+    });
+
+    it('Deve permitir a inserção de dois comentários', () => {
+        render(<PostComment />);
+
+        const commentInput = screen.getByTestId('comment-input');
+        const submitButton = screen.getByTestId('submit-button');
+
+        fireEvent.change(commentInput, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(submitButton);
+
+        expect(screen.getAllByTestId('comment-item').length).toBe(1);
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+
+        fireEvent.change(commentInput, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(submitButton);
+
+        expect(screen.getAllByTestId('comment-item').length).toBe(2);
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
 });
