25ab7eb4bc51a94872f02591a6493e873b798de8
Emerson Teixeira <emerson@exemplo.com>
2024-08-28 15:18:10 -0300

    Corrigindo problemas com a imagem e aplicando uma nova

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://s1.cdn.autoevolution.com/images/news/gallery/1989-batmobile-gets-full-restoration-batman-says-it-s-his-favorite_3.jpg">
+      <Post imageUrl="https://th.bing.com/th/id/OIP.q_lvr7timB6WmfeIfblAdAHaE8?rs=1&pid=ImgDetMain">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
